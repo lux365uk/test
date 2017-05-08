f1e16a11c492e8c47f319d263131e46efc1b864e
Luis Paz <luis@lux365.com>
2017-05-08 12:56:15 +0100

    [HOMEPAGE] Created file index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$title = "This is a TEST Repository";
+$description = "Test repository to try out github functionality";
+
+include "header.php";
+include "main.php";
+include "footer.php";
+
+?>
